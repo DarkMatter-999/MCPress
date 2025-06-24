acb4f59a8703138f92388c6bd297707734939284
DarkMatter-999 <darkmatter999official@gmail.com>
2025-06-24 22:11:01 +0530

    feat: add build rules for blocks

--- webpack.config.js
+++ webpack.config.js
@@ -8,21 +8,45 @@ function generateEntries() {
 
   // Add JS files
   const jsDir = path.resolve(__dirname, "assets/src/js");
-  fs.readdirSync(jsDir).forEach((file) => {
-    if (file.endsWith(".js")) {
-      const name = `js/${file.replace(/\.js$/, "")}`;
-      entries[name] = path.join(jsDir, file);
-    }
-  });
+  if (fs.existsSync(jsDir)) {
+    fs.readdirSync(jsDir).forEach((file) => {
+      if (file.endsWith(".js")) {
+        const name = `js/${file.replace(/\.js$/, "")}`;
+        entries[name] = path.join(jsDir, file);
+      }
+    });
+  }
 
   // Add SCSS files
   const cssDir = path.resolve(__dirname, "assets/src/css");
-  fs.readdirSync(cssDir).forEach((file) => {
-    if (file.endsWith(".scss")) {
-      const name = `css/${file.replace(/\.scss$/, "")}`;
-      entries[name] = path.join(cssDir, file);
-    }
-  });
+  if (fs.existsSync(cssDir)) {
+    fs.readdirSync(cssDir).forEach((file) => {
+      if (file.endsWith(".scss")) {
+        const name = `css/${file.replace(/\.scss$/, "")}`;
+        entries[name] = path.join(cssDir, file);
+      }
+    });
+  }
+
+  // Add block files
+  const blocksDir = path.resolve(__dirname, "assets/src/blocks");
+  if (fs.existsSync(blocksDir)) {
+    fs.readdirSync(blocksDir).forEach((blockName) => {
+      const blockPath = path.join(blocksDir, blockName);
+      if (fs.statSync(blockPath).isDirectory()) {
+        const jsFile = path.join(blockPath, "index.js");
+        const scssFile = path.join(blockPath, "index.scss");
+
+        if (fs.existsSync(jsFile)) {
+          entries[`blocks/${blockName}/index`] = jsFile;
+        }
+
+        if (fs.existsSync(scssFile)) {
+          entries[`blocks/${blockName}/style`] = scssFile;
+        }
+      }
+    });
+  }
 
   return entries;
 }
