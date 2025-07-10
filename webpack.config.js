f1cf35127c6dd32026c4edc9cfdadb2cc9821a03
DarkMatter-999 <darkmatter999official@gmail.com>
2025-07-10 19:50:04 +0530

    feat: add typescript support

--- webpack.config.js
+++ webpack.config.js
@@ -10,8 +10,8 @@ function generateEntries() {
 	const jsDir = path.resolve( __dirname, 'assets/src/js' );
 	if ( fs.existsSync( jsDir ) ) {
 		fs.readdirSync( jsDir ).forEach( ( file ) => {
-			if ( file.endsWith( '.js' ) ) {
-				const name = `js/${ file.replace( /\.js$/, '' ) }`;
+			if ( file.endsWith( '.js' ) || file.endsWith( '.ts' ) ) {
+				const name = `js/${ file.replace( /\.(js|ts)$/, '' ) }`;
 				entries[ name ] = path.join( jsDir, file );
 			}
 		} );
@@ -34,10 +34,13 @@ function generateEntries() {
 		fs.readdirSync( blocksDir ).forEach( ( blockName ) => {
 			const blockPath = path.join( blocksDir, blockName );
 			if ( fs.statSync( blockPath ).isDirectory() ) {
+				const tsFile = path.join( blockPath, 'index.ts' );
 				const jsFile = path.join( blockPath, 'index.js' );
 				const scssFile = path.join( blockPath, 'index.scss' );
 
-				if ( fs.existsSync( jsFile ) ) {
+				if ( fs.existsSync( tsFile ) ) {
+					entries[ `blocks/${ blockName }/index` ] = tsFile;
+				} else if ( fs.existsSync( jsFile ) ) {
 					entries[ `blocks/${ blockName }/index` ] = jsFile;
 				}
 
@@ -53,6 +56,25 @@ function generateEntries() {
 
 module.exports = {
 	...defaultConfig,
+	module: {
+		...defaultConfig.module,
+		rules: [
+			...( defaultConfig.module?.rules || [] ),
+			{
+				test: /\.(ts|tsx)$/,
+				exclude: /node_modules/,
+				use: 'ts-loader',
+			},
+		],
+	},
+	resolve: {
+		...defaultConfig.resolve,
+		extensions: [
+			...( defaultConfig.resolve?.extensions || [] ),
+			'.ts',
+			'.tsx',
+		].filter( ( value, index, self ) => self.indexOf( value ) === index ),
+	},
 	entry: generateEntries(),
 	output: {
 		path: path.resolve( __dirname, 'assets/build' ),
