e5a0acbc4b3d3d6a472157c0e018d89bc285bf73
DarkMatter-999 <darkmatter999official@gmail.com>
2025-07-10 20:15:09 +0530

    fix: change version

--- mcpress.php
+++ mcpress.php
@@ -3,7 +3,7 @@
  * Plugin Name:       MCPress
  * Plugin URI:        https://github.com/DarkMatter-999/MCPress
  * Description:       Model Context Protocol (MCP) Plugin for WordPress. Automate your tasks easily
- * Version:           1.0.0
+ * Version:           0.0.1
  * Requires at least: 6.5
  * Requires PHP:      7.4
  * Author:            DarkMatter-999, Adi-ty, USERSATOSHI, yashjawale
@@ -15,7 +15,7 @@
  *
  * @category Plugin
  * @package  MCPress
- * @author   DarkMatter-999, Adi-ty, USERSATOSHI, yashjawale <>
+ * @author   DarkMatter-999, Adi-ty, USERSATOSHI, yashjawale
  * @license  GPL v2 or later <https://www.gnu.org/licenses/gpl-2.0.html>
  * @link     https://github.com/DarkMatter-999/MCPress
  */
